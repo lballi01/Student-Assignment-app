4dd4d5f5fec08734d4fb708c24c388db1e8a2e31
Leonidas Balli <leonidas.balli@outlook.com>
2024-03-16 20:47:49 +0000

    Backend connected to databse

---
+++ AssignmentSubmissionApp/src/test/java/com/StudentAssignment/AssignmentSubmissionApp/AssignmentSubmissionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StudentAssignment.AssignmentSubmissionApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentSubmissionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
